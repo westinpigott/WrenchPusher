6c32ebabbbcc507c4f4e801c47a484007d9fca6b
Westin Pigott <westinpigott@gmail.com>
2013-01-27 17:46:57 -0600

    First semi-functioning system

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+//initialize autloaders
+require_once 'SplClassLoader.php';
+$classLoader = new SplClassLoader('WrenchPusher', 'lib');
+$classLoader->register();
+$classLoader = new SplClassLoader('Wrench', 'vendor/Wrench/lib');
+$classLoader->register();
+
+//create the pusher connection
+$client = new WrenchPusher\PusherClient('7e03c5482047aa273663', array(
+            'isSecure' => TRUE,
+            'keepAliveDuration' => 5,
+        ));
+$client->connect();
+
+//subscribe to a channel
+$channelName = 'test_channel';
+$client->subscribeToChannel($channelName) or die("Error Subscribing to channel.");
+echo 'Subscribed to channel: ' . $channelName . "\n";
+
+//let it listen forever
+while (true) {
+    $client->keepAlive();
+    $responses = $client->receive();
+    foreach ($responses as $response) {
+        /* @var $response WrenchPusher\PusherMessage */
+        echo 'Event:' . $response->getEvent() . "\n";
+        echo 'Channel:' . $response->getChannel() . "\n";
+        echo 'Data: ';
+        $data = $response->getData();
+        if (is_array($data) || ($data instanceof Traversable) || ($data instanceof stdClass)) {
+            echo "\n";
+            foreach ($data as $id => $row) {
+                echo '    ' . $id . ' : ' . $row . "\n";
+            }
+        } else {
+            echo $data."\n";
+        }
+        echo "--------------------------------------------------------\n";
+    }
+}
+?>
